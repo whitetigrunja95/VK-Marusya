e99e17cc77c3e2d0595430e5e5a21dedca5e43a1
whitetigrunja95 <alinkalovejazz@gmail.com>
2026-03-10 15:40:06 +0300

    Добавлен fallback-маршрут для несуществующих страниц

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from "react-router-dom"
-import { HomePage } from "../pages/HomePage/HomePage"
-import { GenresPage } from "../pages/GenresPage/GenresPage"
-import { GenreMoviesPage } from "../pages/GenreMoviesPage/GenreMoviesPage"
-import { MoviePage } from "../pages/MoviePage/MoviePage"
-import { AccountPage } from "../pages/AccountPage/AccountPage"
+import { Navigate, Route, Routes } from "react-router-dom";
+import { HomePage } from "../pages/HomePage/HomePage";
+import { GenresPage } from "../pages/GenresPage/GenresPage";
+import { GenreMoviesPage } from "../pages/GenreMoviesPage/GenreMoviesPage";
+import { MoviePage } from "../pages/MoviePage/MoviePage";
+import { AccountPage } from "../pages/AccountPage/AccountPage";
 
 export const AppRouter = () => {
   return (
@@ -13,6 +13,7 @@ export const AppRouter = () => {
       <Route path="/genres/:genre" element={<GenreMoviesPage />} />
       <Route path="/movie/:id" element={<MoviePage />} />
       <Route path="/account" element={<AccountPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  )
-}
+  );
+};
